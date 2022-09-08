e3639e8bc4ff7c41c12c011384ee0c22fbaf37b7
Satsuki Akiba <anosatsuk124@gmail.com>
2022-09-08 12:41:14 +0900

    modified: applied styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Editor from './components/Editor';
+import './app.scss';
+
 const App = () => {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Editor />
+        </div>
+    );
 };
 
 export default App;
